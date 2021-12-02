40bc7457a8de99f6ba8d189fcf653ce1ce33a45e
Ankit Kumar <ankitlucky.kumar99@gmail.com>
2021-12-02 17:30:11 +0100

    Fixes bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,47 @@
-import { useState, React } from 'react';
+import { useState, useEffect } from 'react';
 import './app.css';
 import Card from './components/card/Card';
 import Navbar from './components/navbar/Navbar';
 import { posts } from './data/data';
+import { io } from 'socket.io-client';
 
-function App() {
+const App = () => {
 	const [username, setUsername] = useState('');
 	const [user, setUser] = useState('');
-	console.log(user);
+	const [socket, setSocket] = useState(null);
+
+	useEffect(() => {
+		setSocket(io('http://localhost:3003'));
+	}, []);
+
+	const sigInUser = () => {
+		setUser(username);
+		socket.emit('newUser', username);
+	};
+
 	return (
 		<div className="container">
 			{user ? (
 				<>
-					<Navbar />
+					<Navbar socket={socket} />
 					{posts.map((post) => (
-						<Card key={post.id} post={post} />
+						<Card key={post.id} post={post} socket={socket} user={user} />
 					))}
-
 					<span className="username">{user}</span>
 				</>
 			) : (
 				<div className="login">
+					<h2>Notifications App</h2>
 					<input
-						placeholder="username"
 						type="text"
-						onChange={(event) => {
-							setUsername(event.target.value);
-						}}
+						placeholder="username"
+						onChange={(e) => setUsername(e.target.value)}
 					/>
-					<button className="loginBtn" onClick={() => setUser(username)}>
-						Login
-					</button>
+					<button onClick={sigInUser}>Login</button>
 				</div>
 			)}
 		</div>
 	);
-}
+};
 
 export default App;
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,16 +1,23 @@
 import './card.css';
-import { useState } from 'react';
-import HeartFilled from '../../img/heartFilled.svg';
 import Heart from '../../img/heart.svg';
+import HeartFilled from '../../img/heartFilled.svg';
 import Comment from '../../img/comment.svg';
 import Share from '../../img/share.svg';
 import Info from '../../img/info.svg';
+import { useState } from 'react';
 
-function Card({ post }) {
+const Card = ({ post, socket, user }) => {
 	const [liked, setLiked] = useState(false);
-	const handleNotification = () => {
-		setLiked(true);
+
+	const handleNotification = (type) => {
+		type === 1 && setLiked(true);
+		socket.emit('sendNotification', {
+			senderName: user,
+			receiverName: post.username,
+			type,
+		});
 	};
+
 	return (
 		<div className="card">
 			<div className="info">
@@ -26,16 +33,25 @@ function Card({ post }) {
 						src={Heart}
 						alt=""
 						className="cardIcon"
-						onClick={handleNotification}
+						onClick={() => handleNotification(1)}
 					/>
 				)}
-
-				<img src={Comment} alt="" className="cardIcon" />
-				<img src={Share} alt="" className="cardIcon" />
+				<img
+					src={Comment}
+					alt=""
+					className="cardIcon"
+					onClick={() => handleNotification(2)}
+				/>
+				<img
+					src={Share}
+					alt=""
+					className="cardIcon"
+					onClick={() => handleNotification(3)}
+				/>
 				<img src={Info} alt="" className="cardIcon infoIcon" />
 			</div>
 		</div>
 	);
-}
+};
 
 export default Card;
